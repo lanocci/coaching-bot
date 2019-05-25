45f98494d15f5b78b832953239263fe53e0c429f
lanocci <kurn17.kkum@gmail.com>
2019-05-25 20:40:18 +0900

    fix connectivity

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.debug("Request body: " + body)
+    app.logger.info("Request body: " + body)
 
     # handle webhook body
     try:
